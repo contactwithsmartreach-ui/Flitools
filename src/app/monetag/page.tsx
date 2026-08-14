21f578febc1f6f774cf6435bca7732fa56da1956
[dyad] <contactwithsmartreach@gmail.com>
2026-08-14 16:38:23 +0100

    [dyad] Added Monetag site verification file for deployment - wrote 3 file(s)

---
+++ src/app/monetag/page.tsx
@@ -0,0 +1,12 @@
+export default function MonetagVerification() {
+  return (
+    <html>
+      <head>
+        <title>Monetag Verification</title>
+      </head>
+      <body>
+        monetag-verification
+      </body>
+    </html>
+  );
+}
